3830c0e633e924c24a4babc9fbeb2bcc9aa8601a
Murisi Tarusenga <murisit@gmail.com>
2026-01-25 19:09:37 +0200

    Add support for bitstrings and lambda functions.

--- lib/ex2c.ex
+++ lib/ex2c.ex
@@ -1,3 +1,5 @@
+use Bitwise
+
 defmodule Ex2c do
   @moduledoc """
   Documentation for `Ex2c`.
@@ -28,6 +30,10 @@ defmodule Ex2c do
 
   def compile_label({module, function, arity}), do: Atom.to_string(function)
 
+  def compile_goto({:f, 0}), do: {:expr_stmt, {:call_expr, {:symbol_expr, "abort"}, []}}
+
+  def compile_goto(label), do: {:goto_stmt, compile_label(label)}
+
   def compile_literal([]), do: {:call_expr, {:symbol_expr, "make_nil"}, []}
 
   def compile_literal([head | tail]), do: {:call_expr, {:symbol_expr, "make_list"}, [compile_literal(head), compile_literal(tail)]}
@@ -43,6 +49,15 @@ defmodule Ex2c do
 
   def compile_literal(val) when is_number(val), do: {:call_expr, {:symbol_expr, "make_small"}, [{:literal_expr, val}]}
 
+  def compile_literal(bits) when is_bitstring(bits) do
+    size = bit_size(bits)
+    size_rounded_up = (size + 7) &&& ~~~7
+    padded_bits = <<bits::bitstring, 0::size(size_rounded_up - size)>>
+    byte_list = :binary.bin_to_list(padded_bits)
+    byte_array = {:compound_literal_expr, "unsigned char []", Enum.map(byte_list, fn x -> {:expr_initializer, {:literal_expr, x}} end)}
+    {:call_expr, {:symbol_expr, "make_bitstring"}, [{:literal_expr, size}, byte_array]}
+  end
+
   def compile_literal(val), do: {:literal_expr, 0}
 
   def compile_operand({:integer, val}), do: {:call_expr, {:symbol_expr, "make_small"}, [{:literal_expr, val}]}
@@ -51,7 +66,7 @@ defmodule Ex2c do
 
   def compile_operand({:atom, name}), do: compile_literal(name)
 
-  def compile_operand({:x, reg}), do: {:symbol_expr, "x#{reg}"}
+  def compile_operand({:x, reg}), do: {:subscript_expr, {:symbol_expr, "xs"}, {:literal_expr, reg}}
 
   def compile_operand({:y, slot}), do: {:subscript_expr, {:symbol_expr, "E"}, {:literal_expr, slot + 1}}
 
@@ -59,13 +74,11 @@ defmodule Ex2c do
 
   def compile_operand({:tr, val, type}), do: compile_operand(val)
 
-  def compile_operand(_), do: {:literal_expr, 0}
-
   def compile_code(code = {:select_val, selector, fail, {:list, []}}), do: [{:goto_stmt, compile_label(fail)}]
 
   def compile_code(code = {:select_val, selector, fail, {:list, [value, label | rest]}}) do
     [{:comment_stmt, Kernel.inspect(code)},
-     {:if_stmt, {:call_expr, {:symbol_expr, "equal_terms"}, [compile_operand(selector), compile_operand(value)]},
+     {:if_stmt, {:call_expr, {:symbol_expr, "is_eq_exact"}, [compile_operand(selector), compile_operand(value)]},
       [{:goto_stmt, compile_label(label)}], compile_code({:select_val, selector, fail, {:list, rest}})}]
   end
 
@@ -92,7 +105,7 @@ defmodule Ex2c do
   def compile_code(code = {:test, name, label, arguments}) do
     [{:comment_stmt, Kernel.inspect(code)},
      {:if_stmt, {:not_expr, {:call_expr, {:symbol_expr, Atom.to_string(name)}, Enum.map(arguments, &Ex2c.compile_operand/1)}},
-      [{:goto_stmt, compile_label(label)}], []}]
+      [compile_goto(label)], []}]
   end
 
   def compile_code(code = {name = :get_list, source, head, tail}) do
@@ -125,37 +138,72 @@ defmodule Ex2c do
   end
 
   def compile_code(code = {:call, arity, label}) do
-    cargs =  for idx <- 0..(arity-1)//1, do: {:symbol_expr, "x#{idx}"}
+    cargs =  for idx <- 0..(arity-1)//1, do: compile_operand({:x, idx})
     ccall = {:call_expr, {:symbol_expr, compile_label(label)}, cargs}
-    [{:comment_stmt, Kernel.inspect(code)}, {:expr_stmt, {:binary_expr, :=, {:symbol_expr, "x0"}, ccall}}]
+    [{:comment_stmt, Kernel.inspect(code)}, {:expr_stmt, {:binary_expr, :=, compile_operand({:x, 0}), ccall}}]
   end
 
   def compile_code(code = {:call_only, arity, label}) do
-    cargs =  for idx <- 0..(arity-1)//1, do: {:symbol_expr, "x#{idx}"}
+    cargs =  for idx <- 0..(arity-1)//1, do: compile_operand({:x, idx})
     ccall = {:call_expr, {:symbol_expr, compile_label(label)}, cargs}
-    [{:comment_stmt, Kernel.inspect(code)}, {:return_stmt, {:binary_expr, :=, {:symbol_expr, "x0"}, ccall}}]
+    [{:comment_stmt, Kernel.inspect(code)}, {:return_stmt, {:binary_expr, :=, compile_operand({:x, 0}), ccall}}]
   end
 
   def compile_code(code = {:gc_bif, :-, label, live, arguments, reg}) do
     [{:comment_stmt, Kernel.inspect(code)},
      {:if_stmt, {:not_expr, {:call_expr, {:symbol_expr, "bif_sub"}, Enum.map(arguments, &Ex2c.compile_operand/1) ++ [{:address_of_expr, compile_operand(reg)}]}},
-      [{:goto_stmt, compile_label(label)}], []}]
+      [compile_goto(label)], []}]
   end
 
   def compile_code(code = {:gc_bif, :*, label, live, arguments, reg}) do
     [{:comment_stmt, Kernel.inspect(code)},
      {:if_stmt, {:not_expr, {:call_expr, {:symbol_expr, "bif_mul"}, Enum.map(arguments, &Ex2c.compile_operand/1) ++ [{:address_of_expr, compile_operand(reg)}]}},
-      [{:goto_stmt, compile_label(label)}], []}]
+      [compile_goto(label)], []}]
   end
 
   def compile_code(code = {:gc_bif, :+, label, live, arguments, reg}) do
     [{:comment_stmt, Kernel.inspect(code)},
      {:if_stmt, {:not_expr, {:call_expr, {:symbol_expr, "bif_add"}, Enum.map(arguments, &Ex2c.compile_operand/1) ++ [{:address_of_expr, compile_operand(reg)}]}},
-      [{:goto_stmt, compile_label(label)}], []}]
+      [compile_goto(label)], []}]
   end
 
   def compile_code(code = :return) do
-    [{:comment_stmt, Kernel.inspect(code)}, {:return_stmt, {:symbol_expr, "x0"}}]
+    [{:comment_stmt, Kernel.inspect(code)}, {:return_stmt, compile_operand({:x, 0})}]
+  end
+
+  def compile_code(code = {:make_fun3, label, index, unique, dst, {:list, env}}) do
+    cfunc_decl =
+      {:function_declarator,
+       {:pointer_declarator, {:identifier_declarator, ""}}, []}
+    cfunc_type = {:type_name, "struct term", cfunc_decl}
+    [{:comment_stmt, Kernel.inspect(code)},
+     {:expr_stmt, {:binary_expr, :=, compile_operand(dst), {:call_expr, {:symbol_expr, "make_fun"}, [
+      {:cast_expr, cfunc_type, {:address_of_expr, {:symbol_expr, compile_label(label)}}},
+      {:literal_expr, length(env)},
+      {:compound_literal_expr, "struct term []", Enum.map(env, fn x -> {:expr_initializer, compile_operand(x)} end)}
+    ]}}}]
+  end
+
+  def compile_code(code = {:call_fun, arity}) do
+    xs = {:symbol_expr, "xs"}
+    tmp = "tmp"
+    num_free = {:member_access_expr, {:member_access_expr, {:symbol_expr, tmp}, "fun"}, "num_free"}
+    counter = "i"
+    counter_symbol = {:symbol_expr, "i"}
+    env = {:member_access_expr, {:member_access_expr, {:symbol_expr, tmp}, "fun"}, "env"}
+    ptr = {:member_access_expr, {:member_access_expr, {:symbol_expr, tmp}, "fun"}, "ptr"}
+    cargs =  for idx <- 0..(arity-1)//1, do: compile_operand({:x, idx})
+    cparams =  for idx <- 0..(arity-1)//1, do: {"struct term", {:identifier_declarator, ""}}
+    cfunc_decl =
+      {:function_declarator,
+       {:pointer_declarator, {:identifier_declarator, ""}}, cparams}
+    cfunc_type = {:type_name, "struct term", cfunc_decl}
+    [{:comment_stmt, Kernel.inspect(code)},
+     {:declaration_stmt, "struct term", [{{:identifier_declarator, tmp}, {:subscript_expr, xs, {:literal_expr, arity}}}]},
+     {:for_stmt, {:declaration_stmt, "int", [{{:identifier_declarator, counter}, {:literal_expr, 0}}]}, {:binary_expr, :<, counter_symbol, num_free}, {:postfix_expr, :++, counter_symbol},
+      [{:expr_stmt, {:binary_expr, :=, {:subscript_expr, xs, {:binary_expr, :+, counter_symbol, {:literal_expr, arity}}}, {:subscript_expr, env, counter_symbol}}}]},
+     {:expr_stmt, {:call_expr, {:cast_expr, cfunc_type, ptr}, cargs}}
+    ]
   end
 
   def compile_code(code) do
@@ -168,20 +216,24 @@ defmodule Ex2c do
           do:
             {"struct term",
              {:identifier_declarator, "x#{idx}"}}
-
     cfunc_decl =
       {:function_declarator,
        {:identifier_declarator, Atom.to_string(name)}, cparams}
     cfunc_type = {:type_name, "struct term", cfunc_decl}
+    cfunc_prologue =
+      for idx <- 0..(arity-1)//1,
+          do:
+            {:expr_stmt, {:binary_expr, :=, compile_operand({:x, idx}), {:symbol_expr, "x#{idx}"}}}
     cfunc_body = Enum.flat_map(code, &Ex2c.compile_code/1)
-    {:function_stmt, specifier(cfunc_type), cfunc_decl, cfunc_body}
+    [{:comment_stmt, Kernel.inspect({:function, name, arity, entry, []})},
+     {:function_stmt, specifier(cfunc_type), cfunc_decl, cfunc_prologue ++ cfunc_body}]
   end
 
   def compile do
     f = '/home/murisi/Documents/Heliax/Elixir.Playground.beam'
     {:ok, beam} = File.read(f)
     {:beam_file, module, labeled_exports, attributes, compile_info, code} = :beam_disasm.file(beam)
-    IO.puts program_to_string(Enum.map(code, &Ex2c.compile_function/1))
+    IO.puts program_to_string(Enum.flat_map(code, &Ex2c.compile_function/1))
   end
 
   # Convert C expression to string
@@ -207,11 +259,20 @@ defmodule Ex2c do
   def cexpr_to_string({:binary_expr, op, expr1, expr2}),
     do: "(#{cexpr_to_string(expr1)} #{op} #{cexpr_to_string(expr2)})"
 
+  def cexpr_to_string({:postfix_expr, op, expr}),
+    do: "(#{cexpr_to_string(expr)}#{op})"
+
+  def cexpr_to_string({:prefix_expr, op, expr}),
+    do: "(#{op}#{cexpr_to_string(expr)})"
+
   def cexpr_to_string({:not_expr, expr}),
     do: "!#{cexpr_to_string(expr)}"
 
   def cexpr_to_string({:subscript_expr, expr1, expr2}),
     do: "#{cexpr_to_string(expr1)}[#{cexpr_to_string(expr2)}]"
+
+  def cexpr_to_string({:member_access_expr, expr, member}),
+    do: "#{cexpr_to_string(expr)}.#{member}"
 
   def cexpr_to_string({:cast_expr, typename, expr}) do
     "((#{specifier(typename)} #{declarator_to_string(declarator(typename))}) #{cexpr_to_string(expr)})"
@@ -342,6 +403,15 @@ defmodule Ex2c do
 
   def stmt_to_string({:expr_stmt, expr}),
     do: cexpr_to_string(expr) <> ";\n"
+
+  def stmt_to_string({:for_stmt, init_stmt, cond_expr, expr_expr, body}) do
+    str = "for(" <> String.replace(stmt_to_string(init_stmt), "\n", " ") <> cexpr_to_string(cond_expr) <> "; " <> cexpr_to_string(expr_expr) <> ") {\n"
+    str =
+      for stmt <- body, reduce: str do
+        str -> str <> stmt_to_string(stmt)
+      end
+    str <> "}\n"
+  end
 
   def stmt_to_string({:label_stmt, identifier})
       when is_binary(identifier),
